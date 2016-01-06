eb80a491b73b42ffadae9633967a21c65c8eca2f
annadeu3 <annadeu3@gmail.com>
2016-01-06 16:14:09 -0500

    completed project

--- renderer.js
+++ renderer.js
@@ -1,10 +1,21 @@
 var fs = require('fs');
 
- view(templateName, values, response) {
+function mergeValues(values, content) {
+//cycle over the keys
+//replace all {{key}} with the value from the values object
+	for (var key in values) {
+
+		content = content.replace("{{" + key "}}", values[key]);
+	}
+	return content;
+}
+
+ function view(templateName, values, response) {
 	//read from the template files
 	//insert values into the content
 	//write out to the response
-	var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+	var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
+		fileContents = mergeValues(values, fileContents);
 		response.write(fileContents);
 }
 
